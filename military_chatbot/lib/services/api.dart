671084478e4ec7b63696f421f0b515268c170ffd
okxooxoo <okxooxoo@naver.com>
2024-06-17 19:56:25 +0900

    fix: 한글 깨짐 현상 해결

--- military_chatbot/lib/services/api.dart
+++ military_chatbot/lib/services/api.dart
@@ -3,7 +3,8 @@ import 'package:http/http.dart' as http;
 
 class APIService {
   Future<String> post(String message) async {
-    var url = Uri.parse('http://127.0.0.1:8000/receive-data'); // 엔드포인트로 지정해야 함
+    // var url = Uri.parse('http://0.0.0.0:8000/receive-data');
+    var url = Uri.parse('http://165.246.229.36:8000/receive-data'); // 엔드포인트
     try {
       final response = await http.post(
         url,
@@ -12,9 +13,10 @@ class APIService {
           'message': message,
         }),
       );
-      final data = json.decode(response.body);
+      final data = json.decode(utf8.decode(response.bodyBytes));
       return data['your_data'] as String;
     } catch (e) {
+      print(e.toString());
       return e.toString();
     }
   }
